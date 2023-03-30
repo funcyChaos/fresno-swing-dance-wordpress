1e74f018179ce29aa53a9014055e20e4a4f7d0f3
Austin Reilly <areilly@bitwiseindustries.com>
2023-03-30 16:13:26 -0700

    chore(core): add basic wordpress files

---
+++ footer-subscription.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Fresno_Swing_Dance
+ */
+
+?>
+
+	<?php wp_footer(); ?>
+
+	</body>
+</html>
